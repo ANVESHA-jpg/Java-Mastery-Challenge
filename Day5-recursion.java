8a32b035197e18259351399201a8add330906574
Anvesha Srivastava <207409304+ANVESHA-jpg@users.noreply.github.com>
2025-12-23 22:05:08 +0530

    Fix factorial function to handle base case correctly

--- Day5-recursion.java
+++ Day5-recursion.java
@@ -20,6 +20,13 @@ class Result {
      */
 
     public static int factorial(int n) {
+         if(n==0|| n==1)
+    {
+        return 1;
+    }
+    else{
+        return n * factorial(n-1);
+    }
 
     }
 
